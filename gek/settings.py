ea8484cf0c340027771ba0f8b757dcdeb543d670
varunok <varunok13@gmail.com>
2017-02-21 18:16:33 +0200

    add requirements, install psycopg2

--- gek/settings.py
+++ gek/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'gek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gek',
+        'USER': 'gek',
+        'PASSWORD': 'gek',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
