5c6abc5933eacfee45bad57bedc9700eae8d3d0b
varunok <varunok13@gmail.com>
2017-07-16 21:39:55 +0300

    fix random vip tags

--- common/templatetags/vip_tag.py
+++ common/templatetags/vip_tag.py
@@ -7,15 +7,17 @@ from rieltor_object.models import Ofice, Building
 
 register = template.Library()
 
-
 @register.assignment_tag
 def vips():
     office = Ofice.objects.vips().order_by('?')
     building = Building.objects.vips().order_by('?')
     result = list(chain(building, office))
     list_vip = []
-    for i in range(0, 3):
-        rand_item = random.choice(result)
-        list_vip.append(rand_item)
-        result.remove(rand_item)
+    if len(result) > 2:
+        for i in range(0, 3):
+            rand_item = random.choice(result)
+            list_vip.append(rand_item)
+            result.remove(rand_item)
+    else:
+        return result
     return list_vip
